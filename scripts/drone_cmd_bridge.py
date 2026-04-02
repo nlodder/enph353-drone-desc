b905f719af2954dd6b7670139254865421ea8b0c
Nathan Lodder <nathan.lodder@proton.me>
2026-04-02 13:32:14 -0700

    remove planar movement plugin and add force plugin

--- scripts/drone_cmd_bridge.py
+++ scripts/drone_cmd_bridge.py
@@ -1,31 +1,29 @@
 #!/usr/bin/env python3
 import rospy
-from geometry_msgs.msg import Twist
+from geometry_msgs.msg import Wrench, Twist
 from std_msgs.msg import Float64
 
 class DroneCmdBridge:
     def __init__(self):
         rospy.init_node('drone_cmd_bridge')
-        self.UPDATE_HZ = 100
+        self.UPDATE_HZ = 60
 
         # latch last command
-        self.current_twist = Twist()
+        self.current_wrench = Wrench()
 
         # subscribe to drone's cmd_vel topic
         rospy.Subscriber("cmd_vel", Twist, self.cmd_callback)
         # these will become dronex/planar_cmd and dronex/vert_cmd from launch namespacing
-        self.planar_pub = rospy.Publisher("planar_cmd", Twist, queue_size=1)
-        self.vertical_pub = rospy.Publisher("vert_cmd", Float64, queue_size=1)
+        self.mov_pub = rospy.Publisher("cmd_force", Wrench, queue_size=1)
     
     def cmd_callback(self, msg):
-        self.current_twist = msg
+        self.current_wrench = msg
     
     def run(self):
         # rate of cmd updates to planar and vertical movement plugins
         rate = rospy.Rate(self.UPDATE_HZ)
         while not rospy.is_shutdown():
-            self.planar_pub.publish(self.current_twist)
-            self.vertical_pub.publish(Float64(self.current_twist.linear.z))
+            self.mov_pub.publish(self.current_wrench)
             rate.sleep()
 
 if __name__ == '__main__':
