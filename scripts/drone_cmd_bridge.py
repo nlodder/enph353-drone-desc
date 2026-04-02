eb220b0e8c12d73c7550b4a835da06cc5bb18ffb
Nathan Lodder <nathan.lodder@proton.me>
2026-04-02 16:30:22 -0700

    revert changes for testing

--- scripts/drone_cmd_bridge.py
+++ scripts/drone_cmd_bridge.py
@@ -9,21 +9,23 @@ class DroneCmdBridge:
         self.UPDATE_HZ = 60
 
         # latch last command
-        self.current_wrench = Wrench()
+        self.current_twist = Twist()
 
         # subscribe to drone's cmd_vel topic
         rospy.Subscriber("cmd_vel", Twist, self.cmd_callback)
         # these will become dronex/planar_cmd and dronex/vert_cmd from launch namespacing
-        self.mov_pub = rospy.Publisher("cmd_force", Wrench, queue_size=1)
+        self.planar_pub = rospy.Publisher("planar_cmd", Twist, queue_size=1)
+        self.vertical_pub = rospy.Publisher("vert_cmd", Float64, queue_size=1)
     
     def cmd_callback(self, msg):
-        self.current_wrench = msg
+        self.current_twist = msg
     
     def run(self):
         # rate of cmd updates to planar and vertical movement plugins
         rate = rospy.Rate(self.UPDATE_HZ)
         while not rospy.is_shutdown():
-            self.mov_pub.publish(self.current_wrench)
+            self.planar_pub.publish(self.current_twist)
+            self.vertical_pub.publish(Float64(self.current_twist.linear.z))
             rate.sleep()
 
 if __name__ == '__main__':
